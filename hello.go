f09c28f0c236ae35243739e5dc0587b7b0d32467
Ruslan Mamleev <ruslan@mircod.com>
2020-09-20 16:35:12 +0300

    + французский язык

--- hello.go
+++ hello.go
@@ -1,8 +1,10 @@
 package main
 
 const spanish = "Spanish"
+const french = "Spanish"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
@@ -11,6 +13,10 @@ func Hello(name string, language string) string {
 
 	if language == spanish {
 		return spanishHelloPrefix + name
+	}
+
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
