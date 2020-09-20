0d55c7f31a748c2514f2ff7fd3c254df2cf9105c
Ruslan Mamleev <ruslan@mircod.com>
2020-09-20 20:39:11 +0300

    + Stringer, strings

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -1,9 +1,19 @@
 package pointers_and_errors
 
+import "fmt"
+
 type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
 
 type Wallet struct {
 	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
