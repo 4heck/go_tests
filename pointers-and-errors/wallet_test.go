b3f6480d558d15bdb286f47b849ec1e9fc29504d
Ruslan Mamleev <ruslan@mircod.com>
2020-09-20 20:43:45 +0300

    + assertBalance

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -6,45 +6,31 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	t.Run("Deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("Withdraw if has no enough BTC", func(t *testing.T) {
+	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(30))
-
-		got := wallet.Balance()
-
-		want := Bitcoin(20)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(20))
 	})
+
 }
